0c6a814ed603f39aa0b8a7c9cb15f806664c6813
Rishi1314 <rishiraj13052003@gmail.com>
2026-02-10 13:33:22 -0500

    chore: add neon db connection test

---
+++ scripts/db_test.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine,text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+url=os.getenv("DATABASE_URL")
+
+engine = create_engine(url,pool_pre_ping=True)
+
+with engine.connect() as conn:
+    print(conn.execute(text("SELECT 1")).scalar())
